67c39e76d4963a2dc9978764982b01294d513b4c
mzz2017 <m@mzz.pub>
2019-10-22 02:24:13 +0800

    chore: configure PWA

--- gui/src/registerServiceWorker.js
+++ gui/src/registerServiceWorker.js
@@ -21,6 +21,7 @@ if (process.env.NODE_ENV === "production") {
     },
     updated() {
       console.log("New content is available; please refresh.");
+      window.location.reload();
     },
     offline() {
       console.log(
