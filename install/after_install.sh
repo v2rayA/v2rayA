9c4a7aa88e2a8f239589f23a2be19bc4f44c96e0
mzz2017 <m@mzz.pub>
2019-12-28 03:19:35 +0800

    chore

--- install/after_install.sh
+++ install/after_install.sh
@@ -5,6 +5,6 @@ systemctl enable v2raya
 systemctl start v2raya
 echo -e "\033[36m**************************************\033[0m"
 echo -e "\033[36m*         Congratulations!           *\033[0m"
-echo -e "\033[36m* HTTP  demo: http://v.mzz.pub       *\033[0m"
 echo -e "\033[36m* HTTPS demo: https://v2raya.mzz.pub *\033[0m"
+echo -e "\033[36m* HTTP  demo: http://v.mzz.pub       *\033[0m"
 echo -e "\033[36m**************************************\033[0m"
