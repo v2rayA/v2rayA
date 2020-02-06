d77a30ad80135eff19f05c0b8d830270549429f1
mzz2017 <m@mzz.pub>
2020-02-06 12:22:46 +0800

    chore(#29): fix problem that sometimes show '-e' in different types of shells after install

--- install/after_install.sh
+++ install/after_install.sh
@@ -11,8 +11,15 @@ cp $ICON_SOURCE/android-chrome-512x512.png $ICON_TARGET/v2raya.png || true
 
 cp install/v2raya.desktop /usr/share/applications/v2raya.desktop || true
 
-echo -e "\033[36m**************************************\033[0m"
-echo -e "\033[36m*         Congratulations!           *\033[0m"
-echo -e "\033[36m* HTTPS demo: https://v2raya.mzz.pub *\033[0m"
-echo -e "\033[36m* HTTP  demo: http://v.mzz.pub       *\033[0m"
-echo -e "\033[36m**************************************\033[0m"
+ECHOLEN=$(echo -e|awk '{print length($0)}')
+if [ ${ECHOLEN} = '0' ]
+then
+    ECHO='echo -e'
+else
+    ECHO='echo'
+fi;
+$ECHO "\033[36m**************************************\033[0m"
+$ECHO "\033[36m*         Congratulations!           *\033[0m"
+$ECHO "\033[36m* HTTPS demo: https://v2raya.mzz.pub *\033[0m"
+$ECHO "\033[36m* HTTP  demo: http://v.mzz.pub       *\033[0m"
+$ECHO "\033[36m**************************************\033[0m"
