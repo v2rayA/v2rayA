b308bcfc2b035f024cb7fc9be77788670eaf867a
mzz2017 <m@mzz.pub>
2020-02-10 19:05:43 +0800

    feat: add command param --passcheckroot

--- service/global/environmentConfig.go
+++ service/global/environmentConfig.go
@@ -11,6 +11,7 @@ type Params struct {
 	Config        string `id:"config" short:"c" default:"/etc/v2ray/v2raya.json" desc:"V2RayA配置文件路径"`
 	Mode          string `id:"mode" short:"m" desc:"可选systemctl, service, docker, common. 不设置则自动检测"`
 	SSRListenPort int    `short:"s" default:"12346" desc:"使用ss或ssr时的ssr server监听端口，默认12346"`
+	PassCheckRoot bool   `desc:"可跳过启动时的权限检查"`
 }
 
 var params Params
@@ -28,8 +29,9 @@ func initFunc() {
 	}
 }
 
+var once sync.Once
+
 func GetEnvironmentConfig() *Params {
-	var once sync.Once
 	once.Do(initFunc)
 	return &params
 }
