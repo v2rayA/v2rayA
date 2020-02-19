3628920efa815c6a323930ee11469e3163d32685
mzz2017 <m@mzz.pub>
2020-02-20 02:52:06 +0800

    feat: choose locale automatically

--- gui/src/plugins/i18n.js
+++ gui/src/plugins/i18n.js
@@ -7,10 +7,19 @@ Vue.use(VueI18n);
 // Ready translated locale messages
 
 // Create VueI18n instance with options
+let locale = "zh";
+for (let l of window.navigator.languages) {
+  if (l.toLowerCase() === "zh-cn") {
+    l = "zh";
+  }
+  if (l in messages) {
+    locale = l;
+  }
+}
 const i18n = new VueI18n({
-  locale: "en",
-  fallbackLocale: "zh",
-  messages
+  locale,
+  messages,
+  fallbackLocale: "en"
 });
 
 export default i18n;
