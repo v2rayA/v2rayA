6652293739c31c0ba6d649becd4cb9fdc11ebe44
mzz2017 <m@mzz.pub>
2020-02-19 17:13:02 +0800

    docs: ISSUE_TEMPLATE

---
+++ gui/src/locales/en.js
@@ -0,0 +1,36 @@
+export default {
+  delete: "Delete",
+  create: "Create",
+  import: "Import",
+  setting: "Setting",
+  about: "About",
+  loggedAs: "Logged as <b>{username}</b>",
+  logout: "Logout",
+  v2rayCoreStatus: "Status of v2ray-core",
+  checkRunning: "Checking",
+  isRunning: "Running",
+  notRunning: "Stopped",
+  notLogin: "Login please",
+  welcome: {
+    docker: "V2RayA service is running in Docker，Version: {version}",
+    default: "V2RayA service is running，Version: {version}",
+    newVersion: "Detected new version: {version}"
+  },
+  v2ray: {
+    start: "Start",
+    stop: "Stop"
+  },
+  server: {
+    name: "Server Name",
+    address: "Server Address",
+    protocol: "Protocol",
+    latency: "Latency"
+  },
+  subscription: {
+    host: "Host",
+    alias: "Alias",
+    timeLastUpdate: "Datetime of Last Update",
+    numberServers: "Number of Servers"
+  },
+  operations: "Operations"
+};
---
+++ gui/src/locales/zh.js
@@ -0,0 +1,36 @@
+export default {
+  delete: "删除",
+  create: "创建",
+  import: "导入",
+  setting: "设置",
+  about: "关于",
+  loggedAs: "正在以 <b>{username}</b> 的身份登录",
+  logout: "注销",
+  v2rayCoreStatus: "v2ray-core状态",
+  checkRunning: "检测中",
+  isRunning: "正在运行",
+  notRunning: "尚未运行",
+  notLogin: "未登录",
+  welcome: {
+    docker: "V2RayA服务端正在运行于Docker环境中，Version: {version}",
+    default: "V2RayA服务端正在运行，Version: {version}",
+    newVersion: "检测到新版本: {version}"
+  },
+  v2ray: {
+    start: "启动",
+    stop: "关闭"
+  },
+  server: {
+    name: "节点名",
+    address: "节点地址",
+    protocol: "协议",
+    latency: "时延"
+  },
+  subscription: {
+    host: "域名",
+    alias: "别名",
+    timeLastUpdate: "上次更新时间",
+    numberServers: "节点数"
+  },
+  operation: "操作"
+};
