ae05428514b213160cc22001ced2d73faa9bc828
mzz2017 <m@mzz.pub>
2020-03-27 14:59:48 +0800

    test: remove test code

--- service/main.go
+++ service/main.go
@@ -3,22 +3,9 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
-	"os"
-	"runtime/pprof"
 )
 
 func main() {
-	f, err := os.Create("/tmp/v2raya.prof")
-	if err != nil {
-		panic(err)
-	}
-	err = pprof.StartCPUProfile(f)
-	if err != nil {
-		panic(err)
-	}
-	defer pprof.StopCPUProfile()
-
-
 	gin.SetMode(gin.ReleaseMode)
 	checkEnvironment()
 	initConfigure()
