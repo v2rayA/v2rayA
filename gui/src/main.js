27794cb7062e9e79a516766252976b4de1fc5c3d
mzz2017 <m@mzz.pub>
2020-04-14 12:24:51 +0800

    feat: by default, inspect frontend url and set resonable backend address

--- gui/src/main.js
+++ gui/src/main.js
@@ -1,40 +1,11 @@
 import "@/plugins/buefy";
 import "@/plugins/axios";
-import i18n from "@/plugins/i18n";
+import "@/plugins/backendPort";
+import "@/plugins/mount";
 import Vue from "vue";
-import App from "./App.vue";
-import store from "./store";
 import "normalize.css";
 import "@/registerServiceWorker";
 import "@/assets/pace-progressbar";
 import "@/assets/pace-progressbar/themes/blue/pace-theme-corner-indicator.css";
 
 Vue.config.productionTip = false;
-
-let vue = null;
-
-Vue.prototype.$remount = () => {
-  function f(node) {
-    if (!node) {
-      return;
-    }
-    if (typeof node.close == "function") {
-      node.close();
-      return;
-    }
-    if (!("$children" in node)) {
-      return;
-    }
-    for (let i in node.$children) {
-      f(node.$children[i]);
-    }
-  }
-  f(vue);
-  vue = new Vue({
-    i18n,
-    store,
-    render: h => h(App)
-  }).$mount("#app");
-};
-
-Vue.prototype.$remount();
