7f8887f5438a782fdb69f992f1f26f32c6d10649
mzz2017 <m@mzz.pub>
2020-06-07 00:34:41 +0800

    chore: fix some problems

--- service/pre.go
+++ service/pre.go
@@ -135,7 +135,7 @@ func initConfigure() {
 	//db
 	confPath := global.GetEnvironmentConfig().Config
 	if _, err := os.Stat(confPath); os.IsNotExist(err) {
-		_ = os.MkdirAll(path.Dir(confPath), os.ModeDir|0600)
+		_ = os.MkdirAll(path.Dir(confPath), os.ModeDir|0644)
 	}
 	if configure.IsConfigureNotExists() {
 		// need to migrate?
