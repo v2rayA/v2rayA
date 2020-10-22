fd8c6b143b5b167ad07e2e1a33c881415863f79b
mzz2017 <m@mzz.pub>
2020-10-22 23:16:29 +0800

    optimize: prefer to use a consistent secret to reduce passwd input times

--- service/common/jwt/index.go
+++ service/common/jwt/index.go
@@ -1,17 +1,28 @@
 package jwt
 
 import (
+	"crypto/sha256"
 	"github.com/matoous/go-nanoid"
+	"github.com/v2rayA/v2rayA/db/configure"
 	"log"
 )
 
-var secret string
+var secret []byte
 
 func init() {
 	//屡次启动的secret都不一样
-	id, err := gonanoid.Nanoid()
-	if err != nil {
-		log.Fatal(err)
+	//为了减少输入密码的次数，如果有订阅，secret则为所有订阅地址的hash值
+	if sub := configure.GetSubscriptions(); len(sub) > 0 {
+		sha := sha256.New()
+		for _, s := range sub {
+			sha.Write([]byte(s.Address))
+		}
+		secret = sha.Sum(nil)
+	} else {
+		id, err := gonanoid.Nanoid()
+		if err != nil {
+			log.Fatal(err)
+		}
+		secret = []byte(id)
 	}
-	secret = id
 }
