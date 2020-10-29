de4cfa1c2319a9eda10ef984e797eac515e40a90
mzz2017 <m@mzz.pub>
2020-10-29 13:59:19 +0800

    optimize: add user-agent

--- service/common/httpClient/httpGetUsingSpecificClient.go
+++ service/common/httpClient/httpGetUsingSpecificClient.go
@@ -1,14 +1,19 @@
 package httpClient
 
-import "net/http"
+import (
+	"fmt"
+	"github.com/v2rayA/v2rayA/global"
+	"net/http"
+)
 
 func HttpGetUsingSpecificClient(c *http.Client, url string) (resp *http.Response, err error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return
 	}
-	//FIXME: shadowrocket会有可能不清楚alterid的情况，在后端为soga时aid不一致将无法连接
+	//shadowrocket会有可能不清楚alterid的情况，在后端为soga时aid不一致将无法连接
 	//req.Header.Set("User-Agent", "v2rayA (like shadowrocket)")
+	req.Header.Set("User-Agent", fmt.Sprintf("v2rayA/%v WebRequestHelper", global.Version))
 	if resp, err = c.Do(req); err != nil {
 		resp, err = http.DefaultClient.Do(req)
 	}
