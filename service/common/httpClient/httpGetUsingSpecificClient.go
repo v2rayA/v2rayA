780ab8871bc546822f71320f3e89c924fbe61099
mzz2017 <m@mzz.pub>
2020-12-19 13:57:21 +0800

    fix: a problem that cannot view/edit ipv6 servers

--- service/common/httpClient/httpGetUsingSpecificClient.go
+++ service/common/httpClient/httpGetUsingSpecificClient.go
@@ -11,7 +11,7 @@ func HttpGetUsingSpecificClient(c *http.Client, url string) (resp *http.Response
 	if err != nil {
 		return
 	}
-	//shadowrocket会有可能不清楚alterid的情况，在后端为soga时aid不一致将无法连接
+	//shadowrocket会有可能不清楚alterid的情况，影响aead是否启用的问题
 	//req.Header.Set("User-Agent", "v2rayA (like shadowrocket)")
 	req.Header.Set("User-Agent", fmt.Sprintf("v2rayA/%v WebRequestHelper", global.Version))
 	if resp, err = c.Do(req); err != nil {
