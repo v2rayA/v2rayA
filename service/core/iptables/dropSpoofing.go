9979c3730fbfdb28686bbf20a3c143371e6c59b3
mzz2017 <m@mzz.pub>
2021-04-17 20:17:26 +0800

    fix(iptables): avoid 'Another app is currently holding the xtables lock'

--- service/core/iptables/dropSpoofing.go
+++ service/core/iptables/dropSpoofing.go
@@ -6,21 +6,21 @@ var DropSpoofing dropSpoofing
 
 func (r *dropSpoofing) GetSetupCommands() SetupCommands {
 	commands := `
-iptables -N DROP_SPOOFING
-iptables -A DROP_SPOOFING -p udp --sport 53 -m string --algo bm --hex-string "|00047f|" --from 60 --to 180 -j DROP
-iptables -A DROP_SPOOFING -p udp --sport 53 -m string --algo bm --hex-string "|000400000000|" --from 60 --to 180 -j DROP
-iptables -I INPUT -j DROP_SPOOFING 
-iptables -I FORWARD -j DROP_SPOOFING
+iptables -w 2 -N DROP_SPOOFING
+iptables -w 2 -A DROP_SPOOFING -p udp --sport 53 -m string --algo bm --hex-string "|00047f|" --from 60 --to 180 -j DROP
+iptables -w 2 -A DROP_SPOOFING -p udp --sport 53 -m string --algo bm --hex-string "|000400000000|" --from 60 --to 180 -j DROP
+iptables -w 2 -I INPUT -j DROP_SPOOFING 
+iptables -w 2 -I FORWARD -j DROP_SPOOFING
 `
 	return SetupCommands(commands)
 }
 
 func (r *dropSpoofing) GetCleanCommands() CleanCommands {
 	commands := `
-iptables -D INPUT -j DROP_SPOOFING 
-iptables -D FORWARD -j DROP_SPOOFING 
-iptables -F DROP_SPOOFING
-iptables -X DROP_SPOOFING
+iptables -w 2 -D INPUT -j DROP_SPOOFING 
+iptables -w 2 -D FORWARD -j DROP_SPOOFING 
+iptables -w 2 -F DROP_SPOOFING
+iptables -w 2 -X DROP_SPOOFING
 `
 	return CleanCommands(commands)
 }
--- service/core/iptables/redirect.go
+++ service/core/iptables/redirect.go
@@ -14,7 +14,7 @@ func (r *redirect) AddIPWhitelist(cidr string) {
 	// avoid duplication
 	r.RemoveIPWhitelist(cidr)
 	var commands string
-	commands = fmt.Sprintf(`iptables -t nat -I V2RAY -d %s -j RETURN`, cidr)
+	commands = fmt.Sprintf(`iptables -w 2 -t nat -I V2RAY -d %s -j RETURN`, cidr)
 	if !strings.Contains(cidr, ".") {
 		//ipv6
 		commands = strings.Replace(commands, "iptables", "ip6tables", 1)
@@ -24,37 +24,37 @@ func (r *redirect) AddIPWhitelist(cidr string) {
 
 func (r *redirect) RemoveIPWhitelist(cidr string) {
 	var commands string
-	commands = fmt.Sprintf(`iptables -t mangle -D V2RAY -d %s -j RETURN`, cidr)
+	commands = fmt.Sprintf(`iptables -w 2 -t mangle -D V2RAY -d %s -j RETURN`, cidr)
 	cmds.ExecCommands(commands, false)
 }
 
 func (r *redirect) GetSetupCommands() SetupCommands {
 	commands := `
-iptables -t nat -N V2RAY
+iptables -w 2 -t nat -N V2RAY
 # 出方向白名单端口
-iptables -t nat -A V2RAY -p tcp -m multiport --sports {{TCP_PORTS}} -j RETURN
-iptables -t nat -A V2RAY -d 0.0.0.0/32 -j RETURN
-iptables -t nat -A V2RAY -d 10.0.0.0/8 -j RETURN
-iptables -t nat -A V2RAY -d 100.64.0.0/10 -j RETURN
-iptables -t nat -A V2RAY -d 127.0.0.0/8 -j RETURN
-iptables -t nat -A V2RAY -d 169.254.0.0/16 -j RETURN
-iptables -t nat -A V2RAY -d 172.16.0.0/12 -j RETURN
-iptables -t nat -A V2RAY -d 192.0.0.0/24 -j RETURN
-iptables -t nat -A V2RAY -d 192.0.2.0/24 -j RETURN
-iptables -t nat -A V2RAY -d 192.88.99.0/24 -j RETURN
-iptables -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN
+iptables -w 2 -t nat -A V2RAY -p tcp -m multiport --sports {{TCP_PORTS}} -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 0.0.0.0/32 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 10.0.0.0/8 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 100.64.0.0/10 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 127.0.0.0/8 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 169.254.0.0/16 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 172.16.0.0/12 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 192.0.0.0/24 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 192.0.2.0/24 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 192.88.99.0/24 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN
 # fakedns
-# iptables -t nat -A V2RAY -d 198.18.0.0/15 -j RETURN
-iptables -t nat -A V2RAY -d 198.51.100.0/24 -j RETURN
-iptables -t nat -A V2RAY -d 203.0.113.0/24 -j RETURN
-iptables -t nat -A V2RAY -d 224.0.0.0/4 -j RETURN
+# iptables -w 2 -t nat -A V2RAY -d 198.18.0.0/15 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 198.51.100.0/24 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 203.0.113.0/24 -j RETURN
+iptables -w 2 -t nat -A V2RAY -d 224.0.0.0/4 -j RETURN
 # dnsPoison
-# iptables -t nat -A V2RAY -d 240.0.0.0/4 -j RETURN
-iptables -t nat -A V2RAY -m mark --mark 0xff -j RETURN
-iptables -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 32345
+# iptables -w 2 -t nat -A V2RAY -d 240.0.0.0/4 -j RETURN
+iptables -w 2 -t nat -A V2RAY -m mark --mark 0xff -j RETURN
+iptables -w 2 -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 32345
 
-iptables -t nat -I PREROUTING -p tcp -j V2RAY
-iptables -t nat -I OUTPUT -p tcp -j V2RAY
+iptables -w 2 -t nat -I PREROUTING -p tcp -j V2RAY
+iptables -w 2 -t nat -I OUTPUT -p tcp -j V2RAY
 `
 	if cmds.IsCommandValid("ip6tables") {
 		// FIXME:
@@ -64,25 +64,25 @@ iptables -t nat -I OUTPUT -p tcp -j V2RAY
 		commands += `
 ip6tables -t nat -N V2RAY
 # 出方向白名单端口
-ip6tables -t nat -A V2RAY -p tcp -m multiport --sports {{TCP_PORTS}} -j RETURN
-ip6tables -t nat -A V2RAY -d ::/128 -j RETURN
-ip6tables -t nat -A V2RAY -d ::1/128 -j RETURN
-ip6tables -t nat -A V2RAY -d ::ffff:0:0/96 -j RETURN
-ip6tables -t nat -A V2RAY -d ::ffff:0:0:0/96 -j RETURN
-ip6tables -t nat -A V2RAY -d 64:ff9b::/96 -j RETURN
-ip6tables -t nat -A V2RAY -d 100::/64 -j RETURN
-ip6tables -t nat -A V2RAY -d 2001::/32 -j RETURN
-ip6tables -t nat -A V2RAY -d 2001:20::/28 -j RETURN
-ip6tables -t nat -A V2RAY -d 2001:db8::/32 -j RETURN
-ip6tables -t nat -A V2RAY -d 2002::/16 -j RETURN
-ip6tables -t nat -A V2RAY -d fc00::/7 -j RETURN
-ip6tables -t nat -A V2RAY -d fe80::/10 -j RETURN
-ip6tables -t nat -A V2RAY -d ff00::/8 -j RETURN
-ip6tables -t nat -A V2RAY -m mark --mark 0xff -j RETURN
-ip6tables -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 32345
+ip6tables -w 2 -t nat -A V2RAY -p tcp -m multiport --sports {{TCP_PORTS}} -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d ::/128 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d ::1/128 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d ::ffff:0:0/96 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d ::ffff:0:0:0/96 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d 64:ff9b::/96 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d 100::/64 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d 2001::/32 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d 2001:20::/28 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d 2001:db8::/32 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d 2002::/16 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d fc00::/7 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d fe80::/10 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -d ff00::/8 -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -m mark --mark 0xff -j RETURN
+ip6tables -w 2 -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 32345
 
-ip6tables -t nat -I PREROUTING -p tcp -j V2RAY
-ip6tables -t nat -I OUTPUT -p tcp -j V2RAY
+ip6tables -w 2 -t nat -I PREROUTING -p tcp -j V2RAY
+ip6tables -w 2 -t nat -I OUTPUT -p tcp -j V2RAY
 `
 	}
 	return SetupCommands(commands)
@@ -90,17 +90,17 @@ ip6tables -t nat -I OUTPUT -p tcp -j V2RAY
 
 func (r *redirect) GetCleanCommands() CleanCommands {
 	commands := `
-iptables -t nat -F V2RAY
-iptables -t nat -D PREROUTING -p tcp -j V2RAY
-iptables -t nat -D OUTPUT -p tcp -j V2RAY
-iptables -t nat -X V2RAY
+iptables -w 2 -t nat -F V2RAY
+iptables -w 2 -t nat -D PREROUTING -p tcp -j V2RAY
+iptables -w 2 -t nat -D OUTPUT -p tcp -j V2RAY
+iptables -w 2 -t nat -X V2RAY
 `
 	if cmds.IsCommandValid("ip6tables") {
 		commands += `
-ip6tables -t nat -F V2RAY
-ip6tables -t nat -D PREROUTING -p tcp -j V2RAY
-ip6tables -t nat -D OUTPUT -p tcp -j V2RAY
-ip6tables -t nat -X V2RAY
+ip6tables -w 2 -t nat -F V2RAY
+ip6tables -w 2 -t nat -D PREROUTING -p tcp -j V2RAY
+ip6tables -w 2 -t nat -D OUTPUT -p tcp -j V2RAY
+ip6tables -w 2 -t nat -X V2RAY
 `
 	}
 	return CleanCommands(commands)
