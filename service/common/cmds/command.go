d7e3ab9180d549b3a21d3e208fb1738f0a5cb386
mzz2017 <m@mzz.pub>
2021-05-16 09:50:26 +0800

    fix: replace 'which' with 'command -v'

--- service/common/cmds/command.go
+++ service/common/cmds/command.go
@@ -9,7 +9,7 @@ import (
 func IsCommandValid(command string) bool {
 	out, err := exec.Command("sh", "-c", "type '"+command+"'").CombinedOutput()
 	if err != nil {
-		out, err = exec.Command("sh", "-c", "which '"+command+"'").CombinedOutput()
+		out, err = exec.Command("sh", "-c", "command -v '"+command+"'").CombinedOutput()
 	}
 	return err == nil && len(bytes.TrimSpace(out)) > 0
 }
--- service/global/systemServiceControlMode.go
+++ service/global/systemServiceControlMode.go
@@ -15,10 +15,10 @@ const (
 
 func SetServiceControlMode() (mode SystemServiceControlMode) {
 	// The behaviour of this function has changed after v1.1.4
-	if out, err := exec.Command("sh", "-c", "which systemctl").Output(); err == nil && strings.Contains(string(out), "systemctl") {
+	if out, err := exec.Command("sh", "-c", "command -v systemctl").Output(); err == nil && strings.Contains(string(out), "systemctl") {
 		return SystemctlMode
 	}
-	if out, err := exec.Command("sh", "-c", "which service").Output(); err == nil && strings.Contains(string(out), "service") {
+	if out, err := exec.Command("sh", "-c", "command -v service").Output(); err == nil && strings.Contains(string(out), "service") {
 		return ServiceMode
 	}
 	return UniversalMode
