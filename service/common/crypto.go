118954877879d683aac2fc7f45d8fbe79deb9ed1
mzz2017 <mzz@tuta.io>
2021-07-21 16:51:27 +0800

    feat/fix: support the UUID mapping standard. #211

--- service/common/crypto.go
+++ service/common/crypto.go
@@ -3,9 +3,11 @@ package common
 import (
 	"crypto/hmac"
 	"crypto/md5"
+	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"strings"
@@ -51,4 +53,26 @@ func Base64URLDecode(s string) (string, error) {
 		return saver, err
 	}
 	return string(raw), err
+}
+
+// StringToUUID5 is from https://github.com/XTLS/Xray-core/issues/158
+func StringToUUID5(str string) string {
+	var Nil [16]byte
+	h := sha1.New()
+	h.Write(Nil[:])
+	h.Write([]byte(str))
+	u := h.Sum(nil)[:16]
+	u[6] = (u[6] & 0x0f) | (5 << 4)
+	u[8] = u[8]&(0xff>>2) | (0x02 << 6)
+	buf := make([]byte, 36)
+	hex.Encode(buf[0:8], u[0:4])
+	buf[8] = '-'
+	hex.Encode(buf[9:13], u[4:6])
+	buf[13] = '-'
+	hex.Encode(buf[14:18], u[6:8])
+	buf[18] = '-'
+	hex.Encode(buf[19:23], u[8:10])
+	buf[23] = '-'
+	hex.Encode(buf[24:], u[10:])
+	return string(buf)
 }
