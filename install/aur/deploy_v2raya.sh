a158e01c8196ad9832ad8e5c5c6902d8b063b72e
mzz2017 <mzz@tuta.io>
2021-08-02 17:55:36 +0800

    chore: aur

--- install/aur/deploy_v2raya.sh
+++ install/aur/deploy_v2raya.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p /tmp/prepare/v2raya
 cd /tmp/prepare/v2raya
-cp "$srcdir"/install/aur/v2raya/* ./
-cp "$srcdir"/install/aur/v2raya/.* ./
+cp "$P_DIR"/install/aur/v2raya/* ./
+cp "$P_DIR"/install/aur/v2raya/.* ./
 
 sed -i s/{{pkgver}}/"${VERSION:1}"/g PKGBUILD .SRCINFO
 
@@ -14,4 +14,4 @@ cp -f /tmp/prepare/v2raya/.* ./
 git add .
 git commit -m "release $VERSION"
 git push -u -f origin master
-cd $srcdir #回项目目录
+cd $P_DIR #回项目目录
