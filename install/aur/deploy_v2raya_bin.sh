529aa88c516b06b4de7d6855eb904d311095dada
mzz2017 <mzz@tuta.io>
2021-08-02 17:55:36 +0800

    chore: aur

--- install/aur/deploy_v2raya_bin.sh
+++ install/aur/deploy_v2raya_bin.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-sha1sums_i686=$(sha1sum "$srcdir"/v2raya_linux_386_$VERSION | awk '{print $1}')
-sha1sums_x86_64=$(sha1sum "$srcdir"/v2raya_linux_amd64_$VERSION | awk '{print $1}')
-sha1sums_armv6h=$(sha1sum "$srcdir"/v2raya_linux_arm_$VERSION | awk '{print $1}')
+sha1sums_i686=$(sha1sum "$P_DIR"/v2raya_linux_386_$VERSION | awk '{print $1}')
+sha1sums_x86_64=$(sha1sum "$P_DIR"/v2raya_linux_amd64_$VERSION | awk '{print $1}')
+sha1sums_armv6h=$(sha1sum "$P_DIR"/v2raya_linux_arm_$VERSION | awk '{print $1}')
 sha1sums_armv7h=$sha1sums_armv6h
-sha1sums_aarch64=$(sha1sum "$srcdir"/v2raya_linux_arm64_$VERSION | awk '{print $1}')
-sha_service=$(sha1sum "$srcdir"/install/universal/v2raya.service | awk '{print $1}')
-sha_png=$(sha1sum "$srcdir"/install/universal/v2raya.png | awk '{print $1}')
-sha_desktop=$(sha1sum "$srcdir"/install/universal/v2raya.desktop | awk '{print $1}')
-sha_web=$(sha1sum "$srcdir"/web.tar.gz | awk '{print $1}')
+sha1sums_aarch64=$(sha1sum "$P_DIR"/v2raya_linux_arm64_$VERSION | awk '{print $1}')
+sha_service=$(sha1sum "$P_DIR"/install/universal/v2raya.service | awk '{print $1}')
+sha_png=$(sha1sum "$P_DIR"/install/universal/v2raya.png | awk '{print $1}')
+sha_desktop=$(sha1sum "$P_DIR"/install/universal/v2raya.desktop | awk '{print $1}')
+sha_web=$(sha1sum "$P_DIR"/web.tar.gz | awk '{print $1}')
 
 mkdir -p /tmp/prepare/v2raya-bin
 cd /tmp/prepare/v2raya-bin
-cp "$srcdir"/install/aur/v2raya-bin/* ./
-cp "$srcdir"/install/aur/v2raya-bin/.* ./
-cp "$srcdir"/install/universal/v2raya.desktop ./
-cp "$srcdir"/install/universal/v2raya.png ./
-cp "$srcdir"/install/universal/v2raya.service ./
+cp "$P_DIR"/install/aur/v2raya-bin/* ./
+cp "$P_DIR"/install/aur/v2raya-bin/.* ./
+cp "$P_DIR"/install/universal/v2raya.desktop ./
+cp "$P_DIR"/install/universal/v2raya.png ./
+cp "$P_DIR"/install/universal/v2raya.service ./
 
 sed -i s/{{pkgver}}/"${VERSION:1}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_i686}}/"${sha1sums_i686}"/g PKGBUILD .SRCINFO
@@ -36,4 +36,4 @@ cp -f /tmp/prepare/v2raya-bin/.* ./
 git add .
 git commit -m "release $VERSION"
 git push -u -f origin master
-cd $srcdir #回项目目录
+cd $P_DIR #回项目目录
