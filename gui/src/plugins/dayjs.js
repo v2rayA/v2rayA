904e4ddce4e80ea0d9a298f93add318b06eedd97
mzz2017 <mzz@tuta.io>
2021-08-11 17:58:23 +0800

    fix: use local time for subscription updating datetime

--- gui/src/plugins/dayjs.js
+++ gui/src/plugins/dayjs.js
@@ -1,8 +1,14 @@
+"use strict";
+
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 import Vue from "vue";
 import "dayjs/locale/zh-cn";
 import "dayjs/locale/en";
 
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 Vue.prototype.$dayjs = dayjs;
