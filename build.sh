fbc8b87b09bfeebfcc3b50caedcd698796f8762c
mzz2017 <mzz@tuta.io>
2021-08-20 19:44:54 +0800

    chore(build.sh): better locating

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
 set -ex
-P_DIR=$(pwd)
-cd $P_DIR/gui && yarn && OUTPUT_DIR=$P_DIR/service/server/router/web yarn build
-cd $P_DIR/service && go build -o ../v2raya
+SHELL_FOLDER=$(dirname "$0")
+cd "$SHELL_FOLDER"/gui && yarn && OUTPUT_DIR="$SHELL_FOLDER"/service/server/router/web yarn build
+cd "$SHELL_FOLDER"/service && go build -o "$SHELL_FOLDER"/v2raya
