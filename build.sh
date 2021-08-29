00b778288a033f6f3dc2c6b75ce24690ee0f491e
mzz2017 <mzz@tuta.io>
2021-08-29 22:14:59 +0800

    chore: build script cannot work in some cases and adjust the position of waiting network

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
-SHELL_FOLDER=$(dirname "$0")
+SHELL_FOLDER="$(pwd)"/"$(dirname $0)"
 cd "$SHELL_FOLDER"/gui && yarn && OUTPUT_DIR="$SHELL_FOLDER"/service/server/router/web yarn build
 cd "$SHELL_FOLDER"/service && go build -o "$SHELL_FOLDER"/v2raya
