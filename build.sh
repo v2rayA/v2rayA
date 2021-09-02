b4a3967ceca00d14ec13667d201bc15a7a7422fe
mzz2017 <m@mzz.pub>
2021-09-02 19:28:02 +0800

    chore: build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -ex
 SHELL_FOLDER="$(pwd)"/"$(dirname $0)"
 cd "$SHELL_FOLDER"/gui && yarn && OUTPUT_DIR="$SHELL_FOLDER"/service/server/router/web yarn build
-cd "$SHELL_FOLDER"/service && go build -o "$SHELL_FOLDER"/v2raya
+cd "$SHELL_FOLDER"/service && CGO_ENABLED=0 go build -ldflags "-s -w" -o "$SHELL_FOLDER"/v2raya
