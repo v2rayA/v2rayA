9e1e0f3526463a7334c135e082429bf7736a2f0e
mzz2017 <mzz@tuta.io>
2021-09-07 16:29:42 +0800

    optimize: remove useless port whitelist

--- service/core/v2ray/transparent.go
+++ service/core/v2ray/transparent.go
@@ -2,7 +2,6 @@ package v2ray
 
 import (
 	"fmt"
-	"github.com/v2rayA/v2rayA/common"
 	"github.com/v2rayA/v2rayA/conf"
 	"github.com/v2rayA/v2rayA/core/iptables"
 	"github.com/v2rayA/v2rayA/core/specialMode"
@@ -90,49 +89,7 @@ func CheckAndSetupTransparentProxy(checkRunning bool) (err error) {
 	}
 	setting := configure.GetSettingNotNil()
 	preprocess := func(c *iptables.SetupCommands) {
-		commands := string(*c)
-		//先看要不要把自己的端口加进去
-		selfPort := strings.Split(conf.GetEnvironmentConfig().Address, ":")[1]
-		wl := configure.GetPortWhiteListNotNil()
-		if !wl.Has(selfPort, "tcp") {
-			wl.TCP = append(wl.TCP, selfPort)
-		}
-		lines := strings.Split(commands, "\n")
-		for i, line := range lines {
-			if strings.Contains(line, "{{TCP_PORTS}}") {
-				raw := line
-				lines[i] = ""
-				var grp []string
-				r := wl.TCP
-				for r != nil {
-					grp, r = nextPortsGroup(r, 15)
-					if grp != nil {
-						lines[i] += strings.Replace(raw, "{{TCP_PORTS}}", strings.Join(grp, ","), 1) + "\n"
-					}
-				}
-				lines[i] = strings.TrimSuffix(lines[i], "\n")
-			} else if strings.Contains(line, "{{UDP_PORTS}}") {
-				raw := line
-				lines[i] = ""
-				var grp []string
-				r := wl.UDP
-				for r != nil {
-					grp, r = nextPortsGroup(r, 15)
-					if grp != nil {
-						lines[i] += strings.Replace(raw, "{{UDP_PORTS}}", strings.Join(grp, ","), 1) + "\n"
-					}
-				}
-				lines[i] = strings.TrimSuffix(lines[i], "\n")
-			}
-		}
-		commands = strings.Join(lines, "\n")
-		//if setting.AntiPollution == configure.AntipollutionClosed {
-		//	commands = common.TrimLineContains(commands, "udp")
-		//}
-		if specialMode.ShouldUseSupervisor() || specialMode.ShouldUseFakeDns() {
-			commands = common.TrimLineContains(commands, "198.18.0.0/15")
-		}
-		*c = iptables.SetupCommands(commands)
+
 	}
 	if (!checkRunning || ProcessManager.Running()) && setting.Transparent != configure.TransparentClose {
 		DeleteTransparentProxyRules()
