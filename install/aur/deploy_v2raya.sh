eb03689d3632d0da68509dc0d6391e7cf6a53087
mzz2017 <mzz@tuta.io>
2021-09-16 02:38:16 +0800

    chore: fix aur version

--- install/aur/deploy_v2raya.sh
+++ install/aur/deploy_v2raya.sh
@@ -5,7 +5,7 @@ mkdir -p /tmp/prepare/v2raya
 cd /tmp/prepare/v2raya
 cp -r "$P_DIR"/install/aur/v2raya/. ./
 
-sed -i s/{{pkgver}}/"${VERSION:1}"/g PKGBUILD .SRCINFO
+sed -i s/{{pkgver}}/"$VERSION"/g PKGBUILD .SRCINFO
 
 cd /tmp/
 git clone ssh://aur@aur.archlinux.org/v2raya.git
