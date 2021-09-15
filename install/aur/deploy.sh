cafa5375a845e89fd5c52d4780bdee46c9fbdacd
mzz2017 <mzz@tuta.io>
2021-09-16 02:16:26 +0800

    chore: fix release

--- install/aur/deploy.sh
+++ install/aur/deploy.sh
@@ -4,6 +4,7 @@ set -ex
 eval $(ssh-agent)
 chmod 600 ./install/aur/deploy_key
 ./install/tool/ssh-add_expect ./install/aur/deploy_key
+mkdir -p ~/.ssh
 ssh-keyscan -H aur.archlinux.org >>~/.ssh/known_hosts
 git config --global user.name "$(git show -s --format='%an')"
 git config --global user.email "$(git show -s --format='%ae')"
