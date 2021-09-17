ae1049f5203fd3b4c19cb63303463dbe06499aa4
mzz2017 <mzz@tuta.io>
2021-09-17 17:04:42 +0800

    chore: refine build scripts

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -ex
-SHELL_FOLDER="$(pwd)"/"$(dirname $0)"
+CWD="$(pwd)"
+SHELL_FOLDER="$(dirname $(realpath $0))"
 if [ -d "$SHELL_FOLDER/.git" ]; then
-  date=$(git log -1 --format="%cd" --date=short | sed s/-//g)
-  count=$(git rev-list --count HEAD)
-  commit=$(git rev-parse --short HEAD)
+  date=$(git -C "$SHELL_FOLDER" log -1 --format="%cd" --date=short | sed s/-//g)
+  count=$(git -C "$SHELL_FOLDER" rev-list --count HEAD)
+  commit=$(git -C "$SHELL_FOLDER" rev-parse --short HEAD)
   version="unstable-$date.r${count}.$commit"
 else
   version="unstable"
 fi
 cd "$SHELL_FOLDER"/gui && yarn && OUTPUT_DIR="$SHELL_FOLDER"/service/server/router/web yarn build
-cd "$SHELL_FOLDER"/service && CGO_ENABLED=0 go build -ldflags "-X github.com/v2rayA/v2rayA/conf.Version=$version -s -w" -o "$SHELL_FOLDER"/v2raya
+cd "$SHELL_FOLDER"/service && CGO_ENABLED=0 go build -ldflags "-X github.com/v2rayA/v2rayA/conf.Version=$version -s -w" -o "$CWD"/v2raya
