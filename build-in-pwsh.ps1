169f3a83c2941bf9f844eaf2e93db83efe6ebf38
Markson Hon <50002150+MarksonHon@users.noreply.github.com>
2021-09-17 17:21:58 +0800

    Update build-in-pwsh.ps1 (#260)

--- build-in-pwsh.ps1
+++ build-in-pwsh.ps1
@@ -1,20 +1,15 @@
 Set-PSDebug -Trace 1
 
-## Get current folder
-$CWD = Get-Location
-$shell_path = Resolve-Path -Path $PSCommandPath
-$SHELL_FOLDER = Split-Path $shell_path
+## Check OS
+$TestWinDir = Test-Path $env:windir -ErrorAction Ignore
 
-## Get date
-$DateLong = git -C "$SHELL_FOLDER" log -1 --format="%cd" --date=short
-$Date = $DateLong -replace "-"; ""
+if ([String]::IsNullOrEmpty($TestWinDir)) { 
+    $v2rayaBin = "v2raya"
+}
+else {
+    $v2rayaBin = "v2raya.exe"
+}
 
-## Other info
-$count = git -C "$SHELL_FOLDER" rev-list --count HEAD
-$commit = git -C "$SHELL_FOLDER" rev-parse --short HEAD
-
-## Version
-$version = "unstable-$date.r$count.$commit"
 
 ## Test Git / yarn /Go
 $TestGit = Get-Command git -ErrorAction ignore
@@ -33,25 +28,39 @@ else {
             Write-Output "You don't install golang, please install it and add it to your path."
         }
         else {
-            ## Check OS
-            $TestWinDir = Test-Path $env:windir -ErrorAction Ignore
+            ## Get current folder
+            ## $SHELL_FOLDER = Get-Item -LiteralPath ./ | ForEach-Object  -Process { $_.FullName }
+            $CWD = Get-Location
+            $shell_path = Resolve-Path -Path $PSCommandPath
+            $SHELL_FOLDER = Split-Path $shell_path
 
-            if ([String]::IsNullOrEmpty($TestWinDir)) { 
-                $v2rayaBin = "v2raya"
-            }
-            else {
-                $v2rayaBin = "v2raya.exe"
-            }
+            ## Get date
+            $DateLong = git log -1 --format="%cd" --date=short
+            $Date = $DateLong -replace "-"; ""
+
+            ## Other info
+            $count = git rev-list --count HEAD
+            $commit = git rev-parse --short HEAD
+
+            ## Version
+            $version = "unstable-$date.r$count.$commit"
+
+            ## Disable CGO
             ${env:CGO_ENABLED} = "0"
+
+            ## Set yarn's output path
             ${env:OUTPUT_DIR} = "$SHELL_FOLDER/service/server/router/web"
+
+            ## Build v2rayA
             $guiPath = $SHELL_FOLDER + "/gui"
             Set-Location -path "$guiPath"
             yarn
             yarn build
             $corePath = $SHELL_FOLDER + "/service"
             Set-Location -path "$corePath"
-            go build -ldflags "-X github.com/v2rayA/v2rayA/conf.Version=$version -s -w" -o "$CWD/$v2rayaBin"
+            go build -ldflags "-X github.com/v2rayA/v2rayA/conf.Version=$version -s -w" -o "$SHELL_FOLDER/$v2rayaBin"
         }
     }
 }
-Set-Location -path "$CWD"
+
+Set-Location -Path "$SHELL_FOLDER"
