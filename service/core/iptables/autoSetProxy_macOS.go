7c727ab707046638d64eeef54eb39c377201a6ad
Johnshall <xjlhq0096@gmail.com>
2021-12-26 14:00:28 +0800

    规范接口格式

--- service/core/iptables/autoSetProxy_macOS.go
+++ service/core/iptables/autoSetProxy_macOS.go
@@ -10,6 +10,14 @@ import (
 	"strings"
 )
 
+type ports struct {
+	portHttpWithPac int32
+	portSocks5      int32
+	status          bool
+}
+
+var p ports
+
 // GetNetworkServices 用于获取MacOS设备的 networkservices
 func GetNetworkServices() []string {
 	cmd := exec.Command("/usr/sbin/networksetup", "-listallnetworkservices")
@@ -22,18 +30,18 @@ func GetNetworkServices() []string {
 }
 
 // SetProxyOnMacOS 是应用于MacOS平台的自动配置代理
-func SetProxyOnMacOS(flag bool, portHttpWithPac int32, portSocks5 int32) string {
+func (p *ports) SetProxyOnMacOS() string {
 	NetworkServices := GetNetworkServices()
 	commands := ""
 	switch {
-	case flag == true:
+	case p.status == true:
 		for _, service := range NetworkServices {
 			commands += "/usr/sbin/networksetup" + "-setwebproxystate" + service + "on\n"
 			commands += "/usr/sbin/networksetup" + "-setsocksfirewallproxystate" + service + "on\n"
-			commands += "/usr/sbin/networksetup" + "-setwebproxy" + service + "127.0.0.1 " + string(portHttpWithPac) + "\n"
-			commands += "/usr/sbin/networksetup" + "-setsocksfirewallproxy" + service + "127.0.0.1 " + string(portSocks5) + "\n"
+			commands += "/usr/sbin/networksetup" + "-setwebproxy" + service + "127.0.0.1 " + string(p.portHttpWithPac) + "\n"
+			commands += "/usr/sbin/networksetup" + "-setsocksfirewallproxy" + service + "127.0.0.1 " + string(p.portSocks5) + "\n"
 		}
-	case flag == false:
+	case p.status == false:
 		for _, service := range NetworkServices {
 			commands += "/usr/sbin/networksetup" + "-setwebproxystate" + service + "off\n"
 			commands += "/usr/sbin/networksetup" + "-setsocksfirewallproxystate" + service + "off\n"
@@ -43,7 +51,7 @@ func SetProxyOnMacOS(flag bool, portHttpWithPac int32, portSocks5 int32) string 
 }
 
 // AutoSetProxyOnMac 用于配置代理模块的运行
-func AutoSetProxyOnMac(flag bool, portHttpWithPac int32, portSocks5 int32) {
-	commands := SetProxyOnMacOS(flag, portHttpWithPac, portSocks5)
+func (p *ports) AutoSetProxyOnMac() {
+	commands := p.SetProxyOnMacOS()
 	cmds.ExecCommands(commands, false)
 }
