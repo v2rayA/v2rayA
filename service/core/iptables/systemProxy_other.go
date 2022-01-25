1eafeef243755ce4424997b03dd390f84e05bcba
mzz2017 <mzz@tuta.io>
2022-01-25 21:11:57 +0800

    feat(macOS,windows): support system proxy. Thanks @Johnshall #338

---
+++ service/core/iptables/systemProxy_other.go
@@ -0,0 +1,22 @@
+//go:build linux || openbsd || freebsd
+// +build linux openbsd freebsd
+
+package iptables
+
+import "fmt"
+
+type systemProxy struct{}
+
+var SystemProxy systemProxy
+
+func (p *systemProxy) AddIPWhitelist(cidr string) {}
+
+func (p *systemProxy) RemoveIPWhitelist(cidr string) {}
+
+func (p *systemProxy) GetSetupCommands() Setter {
+	return NewErrorSetter(fmt.Errorf("does not support to configure system proxy on your OS"))
+}
+
+func (p *systemProxy) GetCleanCommands() Setter {
+	return Setter{}
+}
