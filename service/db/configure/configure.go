2e3f87a67a7e4736d158dd83e0bf8b0e8c1cc1e7
qqq-qqqq <32921374+qqq-qqqq@users.noreply.github.com>
2022-02-06 12:56:48 +0800

    feat: Add port for socks5 proxy with rule (#404)
    Co-authored-by: mzz2017 <mzz@tuta.io>

--- service/db/configure/configure.go
+++ service/db/configure/configure.go
@@ -34,10 +34,11 @@ func New() *Configure {
 		Setting:          NewSetting(),
 		Accounts:         map[string]string{},
 		Ports: Ports{
-			Socks5:      20170,
-			Http:        20171,
-			HttpWithPac: 20172,
-			Vmess:       0,
+			Socks5:        20170,
+			Socks5WithPac: 0,
+			Http:          20171,
+			HttpWithPac:   20172,
+			Vmess:         0,
 		},
 		InternalDnsList: nil,
 		ExternalDnsList: nil,
@@ -220,6 +221,7 @@ func GetPortsNotNil() *Ports {
 		p = new(Ports)
 		p.Socks5 = 20170
 		p.Http = 20171
+		p.Socks5WithPac = 0
 		p.HttpWithPac = 20172
 		p.Vmess = 0
 	}
