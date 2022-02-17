21695e472fe9878ba031926cecf2af0beca0c33b
高渐离 <admin@gaojianli.me>
2022-02-17 19:23:34 +0800

    optimize: enable allowInsecure for vless (#414)

--- gui/src/plugins/buefy.js
+++ gui/src/plugins/buefy.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import Buefy from "buefy";
+import { ConfigProgrammatic } from "buefy";
 import "@/assets/scss/buefy.scss";
 import "@mdi/font/css/materialdesignicons.css";
 
 Vue.use(Buefy);
+ConfigProgrammatic.setOptions({
+  defaultProgrammaticPromise: true
+});
