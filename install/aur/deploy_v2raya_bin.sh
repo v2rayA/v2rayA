19fac872bc9c52d760b4481f5f65d5ce81aa3644
mzz2017 <mzz@tuta.io>
2022-03-28 17:10:00 +0800

    fix permission denied

--- install/aur/deploy_v2raya_bin.sh
+++ install/aur/deploy_v2raya_bin.sh
@@ -7,7 +7,7 @@ sha1sums_armv6h=$(sha1sum "$P_DIR"/v2raya_linux_arm_$VERSION | awk '{print $1}')
 sha1sums_armv7h=$sha1sums_armv6h
 sha1sums_aarch64=$(sha1sum "$P_DIR"/v2raya_linux_arm64_$VERSION | awk '{print $1}')
 sha_service=$(sha1sum "$P_DIR"/install/universal/v2raya.service | awk '{print $1}')
-sha_service_at=$(sha1sum "$P_DIR"/install/universal/v2raya@.service | awk '{print $1}')
+sha_service_lite=$(sha1sum "$P_DIR"/install/universal/v2raya-lite.service | awk '{print $1}')
 sha_png=$(sha1sum "$P_DIR"/install/universal/v2raya.png | awk '{print $1}')
 sha_desktop=$(sha1sum "$P_DIR"/install/universal/v2raya.desktop | awk '{print $1}')
 
@@ -17,7 +17,7 @@ cp -r "$P_DIR"/install/aur/v2raya-bin/. ./
 cp "$P_DIR"/install/universal/v2raya.desktop ./
 cp "$P_DIR"/install/universal/v2raya.png ./
 cp "$P_DIR"/install/universal/v2raya.service ./
-cp "$P_DIR"/install/universal/v2raya@.service ./
+cp "$P_DIR"/install/universal/v2raya-lite.service ./
 
 sed -i s/{{pkgver}}/"$VERSION"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_i686}}/"${sha1sums_i686}"/g PKGBUILD .SRCINFO
@@ -26,7 +26,7 @@ sed -i s/{{sha1sums_armv6h}}/"${sha1sums_armv6h}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_armv7h}}/"${sha1sums_armv7h}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_aarch64}}/"${sha1sums_aarch64}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha_service}}/"${sha_service}"/g PKGBUILD .SRCINFO
-sed -i s/{{sha_service_at}}/"${sha_service_at}"/g PKGBUILD .SRCINFO
+sed -i s/{{sha_service_lite}}/"${sha_service_lite}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha_png}}/"${sha_png}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha_desktop}}/"${sha_desktop}"/g PKGBUILD .SRCINFO
 
