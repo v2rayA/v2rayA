91de805155a44c91bb0614ced60a57202a680d29
mzz2017 <mzz@tuta.io>
2022-04-20 13:35:47 +0800

    fix: can't download from google store #285

--- service/db/configure/const.go
+++ service/db/configure/const.go
@@ -74,9 +74,9 @@ const (
 # write your own rules below
 domain(domain:mail.qq.com)->direct
 
-domain(geosite:geolocation-!cn)->proxy
 domain(geosite:google-scholar)->proxy
 domain(geosite:category-scholar-!cn, geosite:category-scholar-cn)->direct
+domain(geosite:geolocation-!cn, geosite:google)->proxy
 domain(geosite:cn)->direct
 ip(geoip:hk,geoip:mo)->proxy
 ip(geoip:private, geoip:cn)->direct`
