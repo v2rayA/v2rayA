d7f9539dc0dd879d17c66c48f8a0ebbcfe944714
mzz <mzz@tuta.io>
2022-04-20 13:00:57 +0800

    chore: move logs to /var/log in accordance with FHS 3.0 #415 (#469)

--- install/universal/after_upgrade.sh
+++ install/universal/after_upgrade.sh
@@ -12,3 +12,6 @@ fi;
     $ECHO "\033[36m******************************\033[0m"
     $ECHO "\033[36m*         Completed!         *\033[0m"
     $ECHO "\033[36m******************************\033[0m"
+    $ECHO
+    $ECHO "\033[36mWARN: v2raya@.service was deprecated; please use user service v2raya-lite.service instead.\033[0m"
+    $ECHO "\033[36m      This does NOT impact v2raya.service users.\033[0m"
