a17f058b7c35a8e57c5f19a9f838b9e5eae4762f
mzz2017 <mzz@tuta.io>
2022-04-24 18:49:43 +0800

    fix: resolv.LookupHost problem and non-A/AAAA/CNAME problem in tproxy #188

--- service/common/resolv/resolver.go
+++ service/common/resolv/resolver.go
@@ -26,7 +26,7 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 	dialer := net.Dialer{Timeout: 1000 * time.Millisecond}
 	DefaultResolver = &net.Resolver{
-		PreferGo:     false,
+		PreferGo:     true,
 		StrictErrors: false,
 		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
 			server := dnsServers[rand.Intn(len(dnsServers))]
