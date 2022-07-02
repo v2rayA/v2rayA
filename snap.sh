6406c62d3e66130e433445b2bf71dc64b8208988
Valeriy Manzhos <47538801+InvisibleRain@users.noreply.github.com>
2022-07-02 12:36:13 +0300

    Add a base for muilt-arch support

--- snap.sh
+++ snap.sh
@@ -19,17 +19,25 @@ cd $P_DIR/install/
 
 
 sed -i.backup "s/@VERSION@/$VERSION/g" snap/snapcraft.yaml
-if [ ! -e "installer_debian_amd64_$VERSION.deb" ]; then
-	wget "https://github.com/v2rayA/v2rayA/releases/download/v$VERSION/installer_debian_amd64_$VERSION.deb" \
-	-O "$P_DIR/installer_debian_amd64_$VERSION.deb"
-	ln -t . ../installer_debian_amd64_$VERSION.deb # Snapcraft doesn't support symlinks, so we have to hardlink
-fi
-snapcraft snap --output v2raya_${VERSION}_amd64.snap
+declare readonly architectures=("amd64") # Add your architectures here
+for ARCH in ${architectures[@]}; do
+	sed -i.backup-arch "s/@ARCH@/$ARCH/g" snap/snapcraft.yaml
+	if [ ! -e "installer_debian_${ARCH}_${VERSION}.deb" ]; then
+	wget "https://github.com/v2rayA/v2rayA/releases/download/v${VERSION}/installer_debian_${ARCH}_${VERSION}.deb" \
+	-O "$P_DIR/installer_debian_${ARCH}_${VERSION}.deb"
+	ln -t . ../installer_debian_${ARCH}_${VERSION}.deb # Snapcraft doesn't support symlinks, so we have to hardlink
+	fi
+	snapcraft snap --output v2raya_${VERSION}_${ARCH}.snap
+	mv -f snap/snapcraft.yaml.backup-arch snap/snapcraft.yaml
+done
 
 
+# Cleanup
+for ARCH in ${architectures[@]}; do
+	rm -f installer_debian_${ARCH}_${VERSION}.deb $P_DIR/installer_debian_${ARCH}_${VERSION}.deb
+	mv v2raya_${VERSION}_${ARCH}.snap $P_DIR/
+done
 mv -f snap/snapcraft.yaml.backup snap/snapcraft.yaml
-rm -f installer_debian_amd64_$VERSION.deb $P_DIR/installer_debian_amd64_$VERSION.deb
-mv v2raya_${VERSION}_amd64.snap $P_DIR/
 
 
 # Should publish snap here, but it's a really good idea to smoke test it by hand before pushing to the stable chanell
