bcb0059a3dacb76ef7b2d37706c9e5705e41e2c3
mzz2017 <mzz@tuta.io>
2022-07-03 18:03:48 +0800

    optimize: organize redirect iptables commands

--- service/core/iptables/redirect.go
+++ service/core/iptables/redirect.go
@@ -30,49 +30,57 @@ func (r *redirect) RemoveIPWhitelist(cidr string) {
 
 func (r *redirect) GetSetupCommands() Setter {
 	commands := `
-iptables -w 2 -t nat -N V2RAY
-iptables -w 2 -t nat -A V2RAY -d 0.0.0.0/32 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 10.0.0.0/8 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 100.64.0.0/10 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 127.0.0.0/8 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 169.254.0.0/16 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 172.16.0.0/12 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 192.0.0.0/24 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 192.0.2.0/24 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 192.88.99.0/24 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN
+iptables -w 2 -t nat -N TP_OUT
+iptables -w 2 -t nat -N TP_PRE
+iptables -w 2 -t nat -N TP_RULE
+iptables -w 2 -t nat -A TP_RULE -d 0.0.0.0/32 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 10.0.0.0/8 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 100.64.0.0/10 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 127.0.0.0/8 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 169.254.0.0/16 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 172.16.0.0/12 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 192.0.0.0/24 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 192.0.2.0/24 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 192.88.99.0/24 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 192.168.0.0/16 -j RETURN
 # fakedns
-# iptables -w 2 -t nat -A V2RAY -d 198.18.0.0/15 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 198.51.100.0/24 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 203.0.113.0/24 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 224.0.0.0/4 -j RETURN
-iptables -w 2 -t nat -A V2RAY -d 240.0.0.0/4 -j RETURN
-iptables -w 2 -t nat -A V2RAY -m mark --mark 0x80/0x80 -j RETURN
-iptables -w 2 -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 32345
+# iptables -w 2 -t nat -A TP_RULE -d 198.18.0.0/15 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 198.51.100.0/24 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 203.0.113.0/24 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 224.0.0.0/4 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -d 240.0.0.0/4 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -m mark --mark 0x80/0x80 -j RETURN
+iptables -w 2 -t nat -A TP_RULE -p tcp -j REDIRECT --to-ports 32345
 
-iptables -w 2 -t nat -I PREROUTING -p tcp -j V2RAY
-iptables -w 2 -t nat -I OUTPUT -p tcp -j V2RAY
+iptables -w 2 -t nat -I PREROUTING -p tcp -j TP_PRE
+iptables -w 2 -t nat -I OUTPUT -p tcp -j TP_OUT
+iptables -w 2 -t nat -A TP_PRE -j TP_RULE
+iptables -w 2 -t nat -A TP_OUT -j TP_RULE
 `
 	if IsIPv6Supported() {
 		commands += `
-ip6tables -t nat -N V2RAY
-ip6tables -w 2 -t nat -A V2RAY -d ::/128 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -d ::1/128 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -d 64:ff9b::/96 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -d 100::/64 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -d 2001::/32 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -d 2001:20::/28 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -d 2001:db8::/32 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -d 2002::/16 -j RETURN
+ip6tables -w 2 -t nat -N TP_OUT
+ip6tables -w 2 -t nat -N TP_PRE
+ip6tables -w 2 -t nat -N TP_RULE
+ip6tables -w 2 -t nat -A TP_RULE -d ::/128 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -d ::1/128 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -d 64:ff9b::/96 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -d 100::/64 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -d 2001::/32 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -d 2001:20::/28 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -d 2001:db8::/32 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -d 2002::/16 -j RETURN
 # fakedns
-# ip6tables -w 2 -t nat -A V2RAY -d fc00::/7 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -d fe80::/10 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -d ff00::/8 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -m mark --mark 0x80/0x80 -j RETURN
-ip6tables -w 2 -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 32345
+# ip6tables -w 2 -t nat -A TP_RULE -d fc00::/7 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -d fe80::/10 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -d ff00::/8 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -m mark --mark 0x80/0x80 -j RETURN
+ip6tables -w 2 -t nat -A TP_RULE -p tcp -j REDIRECT --to-ports 32345
 
-ip6tables -w 2 -t nat -I PREROUTING -p tcp -j V2RAY
-ip6tables -w 2 -t nat -I OUTPUT -p tcp -j V2RAY
+ip6tables -w 2 -t nat -I PREROUTING -p tcp -j TP_PRE
+ip6tables -w 2 -t nat -I OUTPUT -p tcp -j TP_OUT
+ip6tables -w 2 -t nat -A TP_PRE -j TP_RULE
+ip6tables -w 2 -t nat -A TP_OUT -j TP_RULE
 `
 	}
 	return Setter{
@@ -82,17 +90,25 @@ ip6tables -w 2 -t nat -I OUTPUT -p tcp -j V2RAY
 
 func (r *redirect) GetCleanCommands() Setter {
 	commands := `
-iptables -w 2 -t nat -F V2RAY
-iptables -w 2 -t nat -D PREROUTING -p tcp -j V2RAY
-iptables -w 2 -t nat -D OUTPUT -p tcp -j V2RAY
-iptables -w 2 -t nat -X V2RAY
+iptables -w 2 -t nat -F TP_OUT
+iptables -w 2 -t nat -D OUTPUT -p tcp -j TP_OUT
+iptables -w 2 -t nat -X TP_OUT
+iptables -w 2 -t nat -F TP_PRE
+iptables -w 2 -t nat -D PREROUTING -p tcp -j TP_PRE
+iptables -w 2 -t nat -X TP_PRE
+iptables -w 2 -t nat -F TP_RULE
+iptables -w 2 -t nat -X TP_RULE
 `
 	if IsIPv6Supported() {
 		commands += `
-ip6tables -w 2 -t nat -F V2RAY
-ip6tables -w 2 -t nat -D PREROUTING -p tcp -j V2RAY
-ip6tables -w 2 -t nat -D OUTPUT -p tcp -j V2RAY
-ip6tables -w 2 -t nat -X V2RAY
+ip6tables -w 2 -t nat -F TP_OUT
+ip6tables -w 2 -t nat -D OUTPUT -p tcp -j TP_OUT
+ip6tables -w 2 -t nat -X TP_OUT
+ip6tables -w 2 -t nat -F TP_PRE
+ip6tables -w 2 -t nat -D PREROUTING -p tcp -j TP_PRE
+ip6tables -w 2 -t nat -X TP_PRE
+ip6tables -w 2 -t nat -F TP_RULE
+ip6tables -w 2 -t nat -X TP_RULE
 `
 	}
 	return Setter{
