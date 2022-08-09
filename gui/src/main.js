d9d90656a5327213577d667a2cb135bc79390da9
Letu Ren <fantasquex@gmail.com>
2022-08-09 12:56:09 +0800

    Move pace-progressbar to node_modules
    Since there is a package pace-js in npm registry, we shouldn't include
    one in source code repository.

--- gui/src/main.js
+++ gui/src/main.js
@@ -7,7 +7,7 @@ import "@/plugins/virtual-scroll";
 import Vue from "vue";
 import "normalize.css";
 // import "@/registerServiceWorker";
-import "@/assets/pace-progressbar";
-import "@/assets/pace-progressbar/themes/blue/pace-theme-corner-indicator.css";
+import "pace-js";
+import "pace-js/themes/blue/pace-theme-corner-indicator.css";
 
 Vue.config.productionTip = false;
