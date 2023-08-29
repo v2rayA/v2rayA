f9b57d74d4b174ef62f7ff610c4cf94ff897678e
Markson <markson@2333ma.top>
2023-08-29 20:13:17 +0800

    Restart service after upgrading

--- install/universal/after_upgrade.sh
+++ install/universal/after_upgrade.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 systemctl daemon-reload
 
+if [ "$(systemctl is-active v2raya)" = "active" ]; then
+    echo "Restarting v2rayA service..."
+    systemctl restart v2raya
+    echo "v2rayA service restarted"
+fi
+
 ECHOLEN=$(echo -e|awk '{print length($0)}')
-if [ ${ECHOLEN} = '0' ]
+if [ "${ECHOLEN}" = '0' ]
 then
     ECHO='echo -e'
 else
@@ -12,4 +18,3 @@ fi;
     $ECHO "\033[36m******************************\033[0m"
     $ECHO "\033[36m*         Completed!         *\033[0m"
     $ECHO "\033[36m******************************\033[0m"
-    
