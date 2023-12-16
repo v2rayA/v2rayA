9775d892d8050433bad505d50472eccd7fdb6e2c
Markson Hon <markson@localhost.localdomain>
2023-12-17 01:09:56 +0800

    docker_helper.sh: Fix install iptables hooks

--- install/docker/ip6tables.sh
+++ install/docker/ip6tables.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-if [ "$V2RAYA_NFTABLES_SUPPORT" != on ]; then
-    if [ "$IPTABLES_MODE" = "nftables" ]; then
-        /sbin/ip6tables-nft "$@"
-    elif [ "$IPTABLES_MODE" = "legacy" ]; then
-        /sbin/ip6tables-legacy "$@"
-    else
-        /sbin/ip6tables "$@"
-    fi
+if [ "$IPTABLES_MODE" = "nftables" ]; then
+    /sbin/ip6tables-nft "$@"
+elif [ "$IPTABLES_MODE" = "legacy" ]; then
+    /sbin/ip6tables-legacy "$@"
+else
+    /sbin/ip6tables "$@"
 fi
--- install/docker/iptables.sh
+++ install/docker/iptables.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-if [ "$V2RAYA_NFTABLES_SUPPORT" != on ]; then
-    if [ "$IPTABLES_MODE" = "nftables" ]; then
-        /sbin/iptables-nft "$@"
-    elif [ "$IPTABLES_MODE" = "legacy" ]; then
-        /sbin/iptables-legacy "$@"
-    else
-        /sbin/iptables "$@"
-    fi
+if [ "$IPTABLES_MODE" = "nftables" ]; then
+    /sbin/iptables-nft "$@"
+elif [ "$IPTABLES_MODE" = "legacy" ]; then
+    /sbin/iptables-legacy "$@"
+else
+    /sbin/iptables "$@"
 fi
