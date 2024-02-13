6e48f36603c2f3c2092d611fecea624b458ecea0
DimmKG <dimakrm361@gmail.com>
2024-02-13 22:50:36 +0700

    fix(gui): add brazil flag in languages bar

--- gui/src/locales/index.js
+++ gui/src/locales/index.js
@@ -2,10 +2,12 @@ import zh from "./zh";
 import en from "./en";
 import fa from "./fa-ir";
 import ru from './ru';
+import pt from './pt-br';
 
 export default {
   zh,
   en,
   fa,
-  ru
+  ru,
+  pt,
 };
