7144962243e2d4e83ae89b1769053fcf26357a6f
Markson Hon <markson@localhost.local>
2024-07-16 10:50:21 +0800

    aur: remove armv6 of v2raya-bin

--- install/aur/deploy_v2raya_bin.sh
+++ install/aur/deploy_v2raya_bin.sh
@@ -3,7 +3,7 @@ set -ex
 
 sha1sums_i686=$(sha1sum "$P_DIR"/v2raya_linux_x86_$VERSION | awk '{print $1}')
 sha1sums_x86_64=$(sha1sum "$P_DIR"/v2raya_linux_x64_$VERSION | awk '{print $1}')
-sha1sums_armv6h=$(sha1sum "$P_DIR"/v2raya_linux_armv6_$VERSION | awk '{print $1}')
+sha1sums_loong64=$(sha1sum "$P_DIR"/v2raya_linux_loongarch64_$VERSION | awk '{print $1}')
 sha1sums_armv7h=$(sha1sum "$P_DIR"/v2raya_linux_armv7_$VERSION | awk '{print $1}')
 sha1sums_aarch64=$(sha1sum "$P_DIR"/v2raya_linux_arm64_$VERSION | awk '{print $1}')
 sha_service=$(sha1sum "$P_DIR"/install/universal/v2raya.service | awk '{print $1}')
@@ -22,7 +22,7 @@ cp "$P_DIR"/install/universal/v2raya-lite.service ./
 sed -i s/{{pkgver}}/"$VERSION"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_i686}}/"${sha1sums_i686}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_x86_64}}/"${sha1sums_x86_64}"/g PKGBUILD .SRCINFO
-sed -i s/{{sha1sums_armv6h}}/"${sha1sums_armv6h}"/g PKGBUILD .SRCINFO
+sed -i s/{{sha1sums_loong64}}/"${sha1sums_loong64}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_armv7h}}/"${sha1sums_armv7h}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_aarch64}}/"${sha1sums_aarch64}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha_service}}/"${sha_service}"/g PKGBUILD .SRCINFO
