9277177507db660d0f81276ede3ecbdb18dd38bb
mosaicwang <mosaicwang@gmail.com>
2024-09-15 12:05:51 +0800

    2024.9.15
    1.增加启动容器的脚本start-v2raya.sh
    2.将部署目录打包为v2raya.tar.gz 以便快速部署

---
+++ start-v2raya.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+l_container_name=v2raya
+
+l_count=$(docker ps -a --filter "name=$l_container_name" |grep -c "$l_container_name")
+
+if [ $l_count -gt 0 ]; then
+	echo 停止容器$l_container_name
+	docker rm -f $l_container_name
+fi
+
+echo 启动容器$l_container_name
+
+# 启动V2rayA
+
+docker run -d \
+  --restart=always \
+  --privileged \
+  --network=host \
+  --name $l_container_name \
+  -e V2RAYA_LOG_FILE=/storage/v2rayA/logs/v2raya.log \
+  -e V2RAYA_V2RAY_BIN=/usr/local/bin/v2ray \
+  -e V2RAYA_NFTABLES_SUPPORT=on \
+  -e IPTABLES_MODE=nftables \
+  -v /lib/modules:/lib/modules:ro \
+  -v /etc/resolv.conf:/etc/resolv.conf \
+  -v /etc/v2raya:/etc/v2raya \
+  registry.cn-hangzhou.aliyuncs.com/mosaicwang/v2raya:2.2.5.8
