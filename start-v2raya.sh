44242198bd9a77d5dcd64fd2da50a8ad09625365
mosaicwang <mosaicwang@gmail.com>
2024-09-16 11:56:42 +0800

    2024.9.16
    分解Dockerfile为Dockerfile-version

--- start-v2raya.sh
+++ start-v2raya.sh
@@ -18,11 +18,13 @@ docker run -d \
   --privileged \
   --network=host \
   --name $l_container_name \
-  -e V2RAYA_LOG_FILE=/storage/v2rayA/logs/v2raya.log \
-  -e V2RAYA_V2RAY_BIN=/usr/local/bin/v2ray \
+  -e V2RAYA_LOG_FILE=/tmp/logs/v2raya.log \
+  -e V2RAYA_V2RAY_BIN=/usr/bin/v2ray \
   -e V2RAYA_NFTABLES_SUPPORT=on \
   -e IPTABLES_MODE=nftables \
   -v /lib/modules:/lib/modules:ro \
   -v /etc/resolv.conf:/etc/resolv.conf \
   -v /etc/v2raya:/etc/v2raya \
+  -v /storage/v2rayA/logs:/tmp/logs \
+  -v /storage/geodata:/usr/local/share/v2ray \
   registry.cn-hangzhou.aliyuncs.com/mosaicwang/v2raya:2.2.5.8
