601804e18de9e8c26720719f1965882c77fec915
Markson Hon <markson@localhost.local>
2025-02-09 17:28:18 +0800

    docker: fix path of iptables

--- install/docker/ip6tables.sh
+++ install/docker/ip6tables.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "$IPTABLES_MODE" = "nftables" ]; then
-    /sbin/ip6tables-nft "$@"
+    /usr/sbin/ip6tables-nft "$@"
 elif [ "$IPTABLES_MODE" = "legacy" ]; then
-    /sbin/ip6tables-legacy "$@"
+    /usr/sbin/ip6tables-legacy "$@"
 else
-    /sbin/ip6tables "$@"
+    /usr/sbin/sbin/ip6tables "$@"
 fi
