6697d44b18343b6ed5038829f157825d3e2ed116
Markson Pigeonzilla Plus <markson@archlinux.local>
2026-06-30 03:15:25 +0800

    feat(v2raya-bin): add v2raya_core binary to AUR package
    - Add v2raya_core source downloads and checksums for all architectures
      in PKGBUILD and .SRCINFO
    - Install v2raya_core to /usr/bin/v2raya_core in package step
    - Update deploy_v2raya_bin.sh to compute and substitute v2raya_core hashes
    - Fix release workflow: download v2raya_core binaries for hash computation
    - Fix release workflow: use heredoc for SSH key to avoid corruption
    - Fix release workflow: correct ssh-add_expect script path

--- install/aur/deploy_v2raya_bin.sh
+++ install/aur/deploy_v2raya_bin.sh
@@ -2,10 +2,15 @@
 set -ex
 
 sha1sums_i686=$(sha1sum "$P_DIR"/v2raya_linux_x86_$VERSION | awk '{print $1}')
+sha1sums_core_i686=$(sha1sum "$P_DIR"/v2raya_core_linux_x86_$VERSION | awk '{print $1}')
 sha1sums_x86_64=$(sha1sum "$P_DIR"/v2raya_linux_x64_$VERSION | awk '{print $1}')
+sha1sums_core_x86_64=$(sha1sum "$P_DIR"/v2raya_core_linux_x64_$VERSION | awk '{print $1}')
 sha1sums_loong64=$(sha1sum "$P_DIR"/v2raya_linux_loongarch64_$VERSION | awk '{print $1}')
+sha1sums_core_loong64=$(sha1sum "$P_DIR"/v2raya_core_linux_loongarch64_$VERSION | awk '{print $1}')
 sha1sums_armv7h=$(sha1sum "$P_DIR"/v2raya_linux_armv7_$VERSION | awk '{print $1}')
+sha1sums_core_armv7h=$(sha1sum "$P_DIR"/v2raya_core_linux_armv7_$VERSION | awk '{print $1}')
 sha1sums_aarch64=$(sha1sum "$P_DIR"/v2raya_linux_arm64_$VERSION | awk '{print $1}')
+sha1sums_core_aarch64=$(sha1sum "$P_DIR"/v2raya_core_linux_arm64_$VERSION | awk '{print $1}')
 sha_service=$(sha1sum "$P_DIR"/install/universal/v2raya.service | awk '{print $1}')
 sha_service_lite=$(sha1sum "$P_DIR"/install/universal/v2raya-lite.service | awk '{print $1}')
 sha_png=$(sha1sum "$P_DIR"/install/universal/v2raya.png | awk '{print $1}')
@@ -21,10 +26,15 @@ cp "$P_DIR"/install/universal/v2raya-lite.service ./
 
 sed -i s/{{pkgver}}/"$VERSION"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_i686}}/"${sha1sums_i686}"/g PKGBUILD .SRCINFO
+sed -i s/{{sha1sums_core_i686}}/"${sha1sums_core_i686}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_x86_64}}/"${sha1sums_x86_64}"/g PKGBUILD .SRCINFO
+sed -i s/{{sha1sums_core_x86_64}}/"${sha1sums_core_x86_64}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_loong64}}/"${sha1sums_loong64}"/g PKGBUILD .SRCINFO
+sed -i s/{{sha1sums_core_loong64}}/"${sha1sums_core_loong64}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_armv7h}}/"${sha1sums_armv7h}"/g PKGBUILD .SRCINFO
+sed -i s/{{sha1sums_core_armv7h}}/"${sha1sums_core_armv7h}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha1sums_aarch64}}/"${sha1sums_aarch64}"/g PKGBUILD .SRCINFO
+sed -i s/{{sha1sums_core_aarch64}}/"${sha1sums_core_aarch64}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha_service}}/"${sha_service}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha_service_lite}}/"${sha_service_lite}"/g PKGBUILD .SRCINFO
 sed -i s/{{sha_png}}/"${sha_png}"/g PKGBUILD .SRCINFO
